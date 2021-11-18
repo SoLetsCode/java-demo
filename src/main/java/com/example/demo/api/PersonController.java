c1a73cb4e6c9cb2c3f2237b8f7133174672e3e8e
Derek <48116904+SoLetsCode@users.noreply.github.com>
2021-11-18 14:09:21 -0800

    finalizing REST api

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,11 +2,21 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import java.util.List;
+import java.util.UUID;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
 
@@ -18,7 +28,27 @@ public class PersonController {
   }
 
   @PostMapping
-  public void addPerson(@RequestBody Person person) {
+  public void addPerson(@Valid @NonNull @RequestBody Person person) {
     personService.addPerson(person);
+  }
+
+  @GetMapping
+  public List<Person> getAllPeople() {
+    return personService.getAllPeople();
+  }
+
+  @GetMapping(path = "{id}")
+  public Person getPersonById(@PathVariable("id") UUID id) {
+    return personService.getPersonById(id).orElse(null);
+  }
+
+  @DeleteMapping(path = "{id}")
+  public void deletePersonById(@PathVariable("id") UUID id) {
+    personService.deletePerson(id);
+  }
+
+  @PutMapping(path = "{id}")
+  public void updatePersonById(@RequestBody @Valid @NonNull Person personToUpdate, @PathVariable UUID id) {
+    personService.updatePerson(id, personToUpdate);
   }
 }
--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -2,9 +2,11 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
+import javax.validation.constraints.NotBlank;
 
 public class Person {
   private final UUID id;
+  @NotBlank
   private final String name;
 
   public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
